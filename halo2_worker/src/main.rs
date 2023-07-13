9940a678bb590229ae204f79f66ae53d2d66644f
Jarred Parr <jparr721@gmail.com>
2023-07-13 11:52:13 -0400

    initial server propagating

--- halo2_worker/src/main.rs
+++ halo2_worker/src/main.rs
@@ -1,9 +1,22 @@
+use futures::SinkExt;
+use serde_derive::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 use tokio_stream::StreamExt;
-use tokio_util::codec::{BytesCodec, Decoder};
-
+use tokio_util::codec::{Framed, LinesCodec};
 struct Worker {
     listener: TcpListener,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Call {
+    name: String,
+    args: Vec<String>,
+}
+
+impl Call {
+    pub fn serialize(&self) -> Result<String, serde_json::Error> {
+        serde_json::to_string(self)
+    }
 }
 
 impl Worker {
@@ -16,24 +29,35 @@ impl Worker {
     async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         println!("Listening on: {}", self.listener.local_addr()?);
         loop {
-            let (socket, _) = self.listener.accept().await?;
-            tokio::spawn(async move {
-                let mut framed = BytesCodec::new().framed(socket);
+            let (socket, connection) = self.listener.accept().await?;
+            println!("received connection from {:?}", connection);
+            let mut lines = Framed::new(socket, LinesCodec::new());
 
-                while let Some(message) = framed.next().await {
-                    println!("Received message: {:?}", message);
+            // Convert the framed stream into JSON call
+            while let Some(result) = lines.next().await {
+                match result {
+                    Ok(line) => {
+                        let call: Call = match serde_json::from_slice(line.as_bytes()) {
+                            Ok(call) => call,
+                            Err(e) => {
+                                let message = format!("Error parsing fn call: {:?}", e.to_string());
+                                println!("{}", message);
+                                lines.send(message).await.unwrap();
+                                continue;
+                            }
+                        };
+                        println!("Received fn call: {:?}", call);
+                        lines.send(call.serialize().unwrap()).await.unwrap();
+                    }
+                    Err(e) => println!("error decoding {:?}", e),
                 }
-            });
+            }
         }
     }
-}
 
-struct WorkOrder {
-    // The name of the request
-    name: String,
-
-    // The arguments to the request
-    args: Vec<u8>,
+    fn handle_fn_call(&self, call: &Call) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
 }
 
 #[tokio::main]
